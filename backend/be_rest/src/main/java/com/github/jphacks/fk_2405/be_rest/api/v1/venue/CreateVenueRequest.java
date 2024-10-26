c0a7b5c86079210781a4a2feda1d2fae356315bf
Nakayama <k24gjk06@st.kyusan-u.ac.jp>
2024-10-26 16:49:59 +0900

    create_venue

---
+++ backend/be_rest/src/main/java/com/github/jphacks/fk_2405/be_rest/api/v1/venue/CreateVenueRequest.java
@@ -0,0 +1,74 @@
+package com.github.jphacks.fk_2405.be_rest.api.v1.venue;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Calendar;
+
+public class CreateVenueRequest {
+    private String title;
+    private String place;
+    private String password;
+    private Date start_time;
+    private Date end_time;
+    private int talk_time_limit;
+
+    public CreateVenueRequest(){
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getStartTime() {
+        return start_time;
+    }
+
+    public void setStartTime(Date start_time) {
+        this.start_time = start_time;
+    }
+
+    /*
+    public void setStartTime(int year, int month, int day){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        start_time = calendar.getTime();
+    } 
+    */
+
+    public Date getEndTime() {
+        return end_time;
+    }
+
+    public void setEnd(Date end_time) {
+        this.end_time = end_time;
+    }
+
+    public int getTalkTimeLimit() {
+        return talk_time_limit;
+    }
+
+    public void setTalkTimeLimit(int talk_time_limit) {
+        this.talk_time_limit = talk_time_limit;
+    }
+}
---
+++ backend/be_rest/src/main/java/com/github/jphacks/fk_2405/be_rest/api/v1/venue/VenueController.java
@@ -0,0 +1,22 @@
+package com.github.jphacks.fk_2405.be_rest.api.v1.venue;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/v1/venue")
+public class VenueController {
+    @PostMapping("create_venue")
+    public ResponseEntity<CreateVenueResponse> signup(@RequestBody CreateVenueRequest entity) {
+        //TODO: process POST request
+
+        CreateVenueResponse response = new CreateVenueResponse();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}

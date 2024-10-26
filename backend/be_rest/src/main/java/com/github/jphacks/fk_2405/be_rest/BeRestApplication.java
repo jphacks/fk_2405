6161cb720e234db12a5fd8237a618a53f431717c
to-ge <tougei324@gmail.com>
2024-10-26 12:05:47 +0900

    add: be_rest(spring-boot project)

---
+++ backend/be_rest/src/main/java/com/github/jphacks/fk_2405/be_rest/BeRestApplication.java
@@ -0,0 +1,13 @@
+package com.github.jphacks.fk_2405.be_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeRestApplication.class, args);
+	}
+
+}
